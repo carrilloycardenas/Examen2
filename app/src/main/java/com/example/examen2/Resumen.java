e6e99247d701e3e0f026b226e0866241d4da6e1e
daniel <daniel.cardenas112957@gmail.com>
2023-07-14 00:28:17 -0700

    Add activity Resumen

---
+++ app/src/main/java/com/example/examen2/Resumen.java
@@ -0,0 +1,13 @@
+package com.example.examen2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Resumen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resumen);
+    }
+}
